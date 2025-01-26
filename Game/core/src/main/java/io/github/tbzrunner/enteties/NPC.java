bcdbc21b5404e871b1ec1479ce3b8d40bc670f31
TahssinAl <142780536+TahssinAl@users.noreply.github.com>
2025-01-27 00:51:22 +0100

    Added NPC movement logic and collision detection; player resets to spawn on collision.

--- Game/core/src/main/java/io/github/tbzrunner/enteties/NPC.java
+++ Game/core/src/main/java/io/github/tbzrunner/enteties/NPC.java
@@ -1,15 +1,37 @@
 package io.github.tbzrunner.enteties;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class NPC extends BaseSprite {
-    public NPC(Sprite sprite) {
+public class NPC extends Sprite {
+
+    private float speed = 2f;
+    private float startX, endX;
+    private boolean movingRight = true;
+
+    public NPC(Sprite sprite, float startX, float endX) {
         super(sprite);
-        setSize(32, 32);
+        this.startX = startX;
+        this.endX = endX;
+        setPosition(startX, getY());
     }
 
-    @Override
     public void update(float delta) {
-        /* TODO implement NPC behavior*/
+        float newX = getX() + (movingRight ? speed : -speed) * delta;
+
+        // Reverse direction if the NPC reaches bounds
+        if (newX > endX) {
+            newX = endX;
+            movingRight = false;
+        } else if (newX < startX) {
+            newX = startX;
+            movingRight = true;
+        }
+
+        setX(newX);
+    }
+
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
     }
 }
--- Game/core/src/main/java/io/github/tbzrunner/screens/GameScreen.java
+++ Game/core/src/main/java/io/github/tbzrunner/screens/GameScreen.java
@@ -7,14 +7,13 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Rectangle;
 import io.github.tbzrunner.enteties.Player;
-
-import java.util.Iterator;
+import io.github.tbzrunner.enteties.NPC;
 
 public class GameScreen extends ScreenAdapter {
     private OrthographicCamera camera;
@@ -22,35 +21,47 @@ public class GameScreen extends ScreenAdapter {
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
     private Player player;
+    private NPC npc;
 
     private float mapWidth;
     private float mapHeight;
 
+    private Vector2 spawnPoint;
+
     @Override
     public void show() {
         map = new com.badlogic.gdx.maps.tiled.TmxMapLoader().load("maps/level-2.tmx");
-        mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / 32f); // Skalierung für Tile-Einheiten
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
 
         mapWidth = map.getProperties().get("width", Integer.class);
         mapHeight = map.getProperties().get("height", Integer.class);
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 25, 15); // Kamera zeigt 25x15 Tiles
+        camera.setToOrtho(false, 25, 15);
         batch = new SpriteBatch();
 
         Texture playerTexture = new Texture("images/player.png");
         TiledMapTileLayer collisionLayer = (TiledMapTileLayer) map.getLayers().get(0);
 
         player = new Player(new Sprite(playerTexture), collisionLayer);
-        player.setSize(1, 1); // Spielergröße in Tile-Einheiten
+        player.setSize(1, 1);
 
-        Vector2 spawnPoint = findSpawnPoint();
+        spawnPoint = findSpawnPoint();
         if (spawnPoint != null) {
             player.setPosition(spawnPoint.x, spawnPoint.y);
         } else {
-            player.setPosition(4, 7); // Fallback-Position
+            player.setPosition(4, 7);
         }
-        logAllTileProperties();
+
+        Texture npcTexture = new Texture("images/npc.png");
+        npc = new NPC(new Sprite(npcTexture), 23, 27);
+        npc.setSize(1, 1);
+        npc.setPosition(22, 36);
+
+
+        /**
+         * Add later on new character
+         */
     }
 
     private Vector2 findSpawnPoint() {
@@ -59,8 +70,7 @@ public class GameScreen extends ScreenAdapter {
             for (int y = 0; y < layer.getHeight(); y++) {
                 TiledMapTileLayer.Cell cell = layer.getCell(x, y);
                 if (cell != null && cell.getTile() != null) {
-                    MapProperties properties = cell.getTile().getProperties();
-                    if (properties.containsKey("spawnpoint")) {
+                    if (cell.getTile().getProperties().containsKey("spawnpoint")) {
                         return new Vector2(x, y);
                     }
                 }
@@ -73,11 +83,17 @@ public class GameScreen extends ScreenAdapter {
     public void render(float delta) {
         handleInput();
 
+        // Update player and NPC
         player.update(delta);
+        npc.update(delta);
 
-        /**
-         * Cam will follow the player
-         */
+        // Check collision between player and NPC
+        if (checkCollision(player.getBoundingRectangle(), npc.getBoundingRectangle())) {
+            // Reset player to spawn point upon collision
+            player.setPosition(spawnPoint.x, spawnPoint.y);
+        }
+
+        // Update camera to follow player
         camera.position.set(
             player.getX() + player.getWidth() / 2,
             player.getY() + player.getHeight() / 2,
@@ -92,6 +108,7 @@ public class GameScreen extends ScreenAdapter {
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         player.draw(batch);
+        npc.draw(batch);
         batch.end();
     }
 
@@ -109,10 +126,13 @@ public class GameScreen extends ScreenAdapter {
         }
     }
 
+    private boolean checkCollision(Rectangle rect1, Rectangle rect2) {
+        return rect1.overlaps(rect2);
+    }
+
     @Override
     public void resize(int width, int height) {
-        camera.viewportWidth = width / 32f;
-        camera.viewportHeight = height / 32f;
+        camera.setToOrtho(false, width / 32f, height / 32f);
     }
 
     @Override
@@ -121,32 +141,6 @@ public class GameScreen extends ScreenAdapter {
         mapRenderer.dispose();
         batch.dispose();
         player.getTexture().dispose();
-    }
-
-    private void logAllTileProperties() {
-        /**
-         * Take th e first layer of the TiledMap Layer
-         */
-        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
-
-        for (int x = 0; x < layer.getWidth(); x++) {
-            for (int y = 0; y < layer.getHeight(); y++) {
-                TiledMapTileLayer.Cell cell = layer.getCell(x, y);
-                if (cell != null && cell.getTile() != null) {
-                    MapProperties properties = cell.getTile().getProperties();
-                    if (properties != null) {
-                        System.out.println("Tile at (" + x + ", " + y + "):");
-                        for (Iterator<String> it = properties.getKeys(); it.hasNext(); ) {
-                            String key = it.next();
-                            System.out.println("  " + key + " = " + properties.get(key));
-                        }
-                    } else {
-                        System.out.println("Tile at (" + x + ", " + y + ") has no properties.");
-                    }
-                } else {
-                    System.out.println("No tile at (" + x + ", " + y + ").");
-                }
-            }
-        }
+        npc.getTexture().dispose();
     }
 }
